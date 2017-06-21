38a768bcec5e3dcda397540565ca69f7d6c8a8c4
chao <luanxuechaowd@gmail.com>
2017-06-21 11:16:27 +0800

    upd $$message service

--- web/modules/module-main/controller/MenuController.js
+++ web/modules/module-main/controller/MenuController.js
@@ -4,9 +4,14 @@
 moduleMain.controller('MenuController', function ($scope,$rootScope,$state,$$Message) {
 
     $scope.a=function () {
-        $$Message.showConfirm().then(function (isOk) {
-            console.log(isOk);
-        })
+        // $$Message.showConfirm().then(function (isOk) {
+        //     console.log(isOk);
+        // })
+        // $$Message.success('1111','2222');
+        // $$Message.info('1111','2222');
+        // $$Message.showAlert('1111','2222');
+        // $$Message.showError('1111','2222');
+
         // Alert.success('11111','2222');
     }
 
--- web/modules/module-main/service/Message.js
+++ web/modules/module-main/service/Message.js
@@ -4,9 +4,34 @@
 moduleMain.factory('$$Message',function (SweetAlert) {
 
     return {
-        showConfirm:function (title) {
-            return SweetAlert.success("You have successfully completed our poll!", {title: "Good job!"});
+        showConfirm:function (content) {
+            var options= {
+                title: '确认',
+                text: content,
+                type: "warning",
+                showCancelButton: true
+            }
+            return SweetAlert.confirm(content, options);
 
+        },
+        // An alert dialog
+        showAlert: function (content) {
+            var options= {
+                title: '警告',
+                text: content,
+                type: "warning",
+                showCancelButton: false
+            }
+            return SweetAlert.alert(content, options);
+        },
+        showError: function (content, title) {
+            return SweetAlert.error(content, {title: '错误'});
+        },
+        info: function (content) {
+            return SweetAlert.info(content, {title: '提示'});
+        },
+        success:function (content,title) {
+            return SweetAlert.success(content, {title: '成功'});
         }
     }
     
