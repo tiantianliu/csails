78f31b4c55e0b62f93c3292c808b1bb0e3d8a4de
chao <luanxuechaowd@gmail.com>
2017-06-28 12:50:00 +0800

    upd article

---
+++ web/modules/bean/Enums.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chao on 2017/6/28.
+ */
+function Enums() {
+
+}
+
+Enums.ArticleType={
+    QUESTION:'QUESTION',
+    PERFECT:'PERFECT',
+    SHARE:'SHARE',
+    ALL:'ALL'
+}
--- web/modules/module-article/controller/ArticleController.js
+++ web/modules/module-article/controller/ArticleController.js
@@ -1,6 +1,14 @@
 /**
  * Created by chao  on 2017/06/11.
  */
-moduleArticle.controller('AboutController', function ($scope) {
+moduleArticle.controller('ArticleController', function ($scope) {
+    $scope.articleLabel = Enums.ArticleType.ALL;
+    $scope.changeTag = changeTag;
+
+    function changeTag(tag) {
+        if(tag == $scope.articleLabel)
+            return false;
+        $scope.articleLabel = tag;
+    }
 
 });
